2ab4648d4b65e83e03179430366563ba9fd61130
Nobuhiro Oto <nobuhirooto@gmail.com>
2024-12-16 15:35:20 +0900

    spring boot init

---
+++ backend/src/main/java/com/mainichitasks/MainichiTasks/MainichiTasksApplication.java
@@ -0,0 +1,13 @@
+package com.mainichitasks.MainichiTasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainichiTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainichiTasksApplication.class, args);
+	}
+
+}
